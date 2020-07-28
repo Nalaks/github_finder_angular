ad288a5d61db70495d8ef9f32af991c564ca44a0
Stefan <insertinsight@gmail.com>
2020-07-28 13:24:19 +0200

    adding small fixes and type defs

--- src/app/services/user-search.service.ts
+++ src/app/services/user-search.service.ts
@@ -1,42 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { User } from '../interfaces/user-interfaces'
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../interfaces/user-interfaces';
 
-
-const USER_DETAIL_MODEL = 'https://api.github.com/users/'
-const USER_MODEL = 'https://api.github.com/search/users?q='
-const RANDOM_MODEL = 'https://api.github.com/user/'
-const REPO_MODEL = '/repos?per_page=5&sort=created:asc'
+const USER_DETAIL_MODEL = 'https://api.github.com/users/';
+const USER_MODEL = 'https://api.github.com/search/users?q=';
+const RANDOM_MODEL = 'https://api.github.com/user/';
+const REPO_MODEL = '/repos?per_page=5&sort=created:asc';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserSearchService {
-
   private stateSource = new BehaviorSubject(null);
-  currentState= this.stateSource.asObservable();
+  currentState = this.stateSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUserDetails(name: string) {
-    return this.http.get(`${USER_DETAIL_MODEL}${name}`)
+    return this.http.get(`${USER_DETAIL_MODEL}${name}`);
   }
 
-  getUser(name: string) {
-    return this.http.get(`${USER_MODEL}${name}`)
+  getUser(name: string): Observable<object> {
+    return this.http.get(`${USER_MODEL}${name}`);
   }
 
-  getRandom(id: number) {
-    return this.http.get(`${RANDOM_MODEL}${id}`)
+  getRandom(id: number): Observable<object> {
+    return this.http.get(`${RANDOM_MODEL}${id}`);
   }
 
-  getRepos(name: string) {
-    return this.http.get(`${USER_DETAIL_MODEL}${name}${REPO_MODEL}`)
+  getRepos(name: string): Observable<object> {
+    return this.http.get(`${USER_DETAIL_MODEL}${name}${REPO_MODEL}`);
   }
 
-  changeState(state: User[]) {
-    this.stateSource.next(state)
+  changeState(state: User[]): void {
+    this.stateSource.next(state);
   }
 }
-
--- src/app/user-search/user-search.component.ts
+++ src/app/user-search/user-search.component.ts
@@ -1,49 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSearchService } from '../services/user-search.service';
-import { CurrentUser, User } from '../interfaces/user-interfaces'
-
+import { CurrentUser, User } from '../interfaces/user-interfaces';
 
 @Component({
   selector: 'app-user-search',
   templateUrl: './user-search.component.html',
-  styleUrls: ['./user-search.component.scss']
+  styleUrls: ['./user-search.component.scss'],
 })
 export class UserSearchComponent implements OnInit {
-  currentUser: User[] | null = null
-  randomUser: User | null = null
-  inputText: string
-  loading: boolean = false
+  currentUser: User[] | null = null;
+  randomUser: User | null = null;
+  inputText: string;
+  loading = false;
 
-  constructor(private userSearchService: UserSearchService) { }
+  constructor(private userSearchService: UserSearchService) {}
 
-  searchUser(name: string) {
-    this.loading = true
+  searchUser(name: string): void {
+    this.loading = true;
     setTimeout(() => {
-      this.userSearchService.getUser(name).subscribe((result: CurrentUser) => this.currentUser = result.items.slice(0, 20))
-    }, 500)    
+      this.userSearchService
+        .getUser(name)
+        .subscribe(
+          (result: CurrentUser) =>
+            (this.currentUser = result.items.slice(0, 20))
+        );
+    }, 500);
     setTimeout(() => {
-      this.loading = false
-    }, 500)  
+      this.loading = false;
+    }, 500);
   }
 
-  clearUsers() {
-    this.currentUser = null
-    this.inputText = ''
+  clearUsers(): void {
+    this.currentUser = null;
+    this.inputText = '';
   }
 
-  ngOnInit(): void {  
-    this.generateRandomUser()
-    this.userSearchService.currentState.subscribe((state: User[]) => this.currentUser = state)
+  ngOnInit(): void {
+    this.generateRandomUser();
+    this.userSearchService.currentState.subscribe(
+      (state: User[]) => (this.currentUser = state)
+    );
   }
 
-  saveData() {
-    console.log(this.currentUser)
-    this.userSearchService.changeState(this.currentUser)
+  saveData(): void {
+    console.log(this.currentUser);
+    this.userSearchService.changeState(this.currentUser);
   }
 
-  generateRandomUser() {
-    const id = Math.floor((Math.random() * 10000) + 1);
-    this.userSearchService.getRandom(id).subscribe((result: User) => this.randomUser = result)
+  generateRandomUser(): void {
+    const id = Math.floor(Math.random() * 10000 + 1);
+    this.userSearchService
+      .getRandom(id)
+      .subscribe((result: User) => (this.randomUser = result));
   }
 }
-
